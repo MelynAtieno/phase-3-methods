77f0ac08c7e787ef9a22df9a99e0cd8f3b56b02c
Melyn Atieno Were <melynatieno@gmail.com>
2022-08-16 12:04:10 +0300

    Assignment Complete

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name = "Melyn")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default("Andrew")
+
+def add(num1,num2)
+    return num1 + num2
+end
+sum = add(1,1)
+
+def halve(num3)
+    if num3.class != Integer
+        return nil
+    end
+    return num3 / 2
+    
+end
+divide = halve(10)
+
+
+
